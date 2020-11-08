4c96010af90afbf25b97f8c4f0f63eb11def4216
Adam Burry <aburry@ieee.org>
2020-11-07 23:45:18 -0330

    Update RayTracerDsl.elm

--- src/RayTracerDsl.elm
+++ src/RayTracerDsl.elm
@@ -4,7 +4,6 @@ module RayTracerDsl exposing
     , cube
     , cylinder
     , group
-    , insert
     , intersect
     , light
     , material
@@ -28,6 +27,7 @@ module RayTracerDsl exposing
     , vector
     )
 
+import Assembly as A
 import Geometry as G
 import RayTracer as R
 
@@ -60,57 +60,47 @@ objParse =
 
 
 scale x y z =
-    applyTransform (G.matNewScale x y z)
+    A.asmTransform [ G.matNewScale x y z ]
 
 
 rotateX a =
-    applyTransform (G.matNewRotateX a)
+    A.asmTransform [ G.matNewRotateX a ]
 
 
 rotateY a =
-    applyTransform (G.matNewRotateY a)
+    A.asmTransform [ G.matNewRotateY a ]
 
 
 rotateZ a =
-    applyTransform (G.matNewRotateZ a)
+    A.asmTransform [ G.matNewRotateZ a ]
 
 
-translate x y z  =
-    applyTransform (G.matNewTranslate x y z)
+translate x y z =
+    A.asmTransform [ G.matNewTranslate x y z ]
 
 
 group =
-    R.Group []
+    A.Group G.aabbAll []
 
 
 subtract a1 a2 =
-    R.CSG [] R.Difference a2 a1
-
-
-csgFromList op =
-    \assembly ->
-        case assembly of
-            [] ->
-                R.Empty
-
-            x :: xs ->
-                List.foldl (R.CSG [] op) x xs
+    A.CSG G.aabbAll [] A.Difference a2 a1
 
 
 union =
-    csgFromList R.Union
+    csgFromList A.Union
 
 
 intersect =
-    csgFromList R.Intersect
+    csgFromList A.Intersect
 
 
-pairs xs ys =
-    List.concatMap (\fn -> List.map fn ys) (List.map Tuple.pair xs)
+triples =
+    G.triples
 
 
-triples xs ys zs =
-    List.concatMap (\( x, y ) -> List.map (\z -> ( x, y, z )) zs) (pairs xs ys)
+pairs =
+    G.pairs
 
 
 point =
@@ -123,72 +113,43 @@ vector =
 
 cube =
     -- unit cube centered at the origin
-    R.Primitive []
-        { id = 0
-        , geometry = G.shpNewShape G.Cube
-        , material = R.defaultMaterial
-        , worldToObject = G.matNewIdentity
-        }
+    primitive G.Cube
         |> scale 0.5 0.5 0.5
 
 
 plane =
     -- x-z plane
-    R.Primitive []
-        { id = 0
-        , geometry = G.shpNewShape G.Plane
-        , material = R.defaultMaterial
-        , worldToObject = G.matNewIdentity
-        }
+    primitive G.Plane
 
 
 sphere =
     -- sphere (r = 0.5) centered at the origin
-    R.Primitive []
-        { id = 0
-        , geometry = G.shpNewShape G.Sphere
-        , material = R.defaultMaterial
-        , worldToObject = G.matNewIdentity
-        }
+    primitive G.Sphere
         |> scale 0.5 0.5 0.5
 
 
 cylinder =
     -- capped cylinder (r = 0.5, h = 1) centered at the origin, y-axis of rotation
-    R.Primitive []
-        { id = 0
-        , geometry = G.shpNewShape (G.Cylinder { ymin = -1, ymax = 1, capped = True })
-        , material = R.defaultMaterial
-        , worldToObject = G.matNewIdentity
-        }
+    primitive (G.Cylinder { ymin = -1, ymax = 1, capped = True })
         |> scale 0.5 0.5 0.5
 
 
 cone =
     -- capped cone (r = 0.5, h = 1) centered at the origin, y-axis of rotation
-    R.Primitive []
-        { id = 0
-        , geometry = G.shpNewShape (G.Cone { ymin = -1, ymax = 0, capped = True })
-        , material = R.defaultMaterial
-        , worldToObject = G.matNewIdentity
-        }
+    primitive (G.Cone { ymin = -1, ymax = 0, capped = True })
         |> scale 0.5 1 0.5
         |> translate 0 0.5 0
 
 
 triangle =
     -- right-triangle (b = 1, h = 1) in x-z plane, right angle at origin
-    R.Primitive []
-        { id = 0
-        , geometry = G.shpNewShape (G.Triangle { a = point 0 0 0, b = point 1 0 0, c = point 0 0 1 })
-        , material = R.defaultMaterial
-        , worldToObject = G.matNewIdentity
-        }
+    primitive (G.Triangle { a = point 0 0 0, b = point 1 0 0, c = point 0 0 1 })
 
 
 primitive config =
     -- todo what is a nice way to expose the configs
-    R.Primitive []
+    A.Primitive G.aabbAll
+        []
         { id = 0
         , geometry = G.shpNewShape config
         , material = R.defaultMaterial
@@ -200,20 +161,15 @@ material =
     R.applyMaterial
 
 
-applyTransform transform assembly =
-    case assembly of
-        R.Empty ->
-            R.Empty
 
-        R.Primitive transformList shape ->
-            R.Primitive (transformList ++ [ transform ]) shape
-
-        R.CSG transformList op left right ->
-            R.CSG (transformList ++ [ transform ]) op left right
-
-        R.Group transformList list ->
-            R.Group (transformList ++ [ transform ]) list
+{- Helpers -}
 
 
-insert =
-    R.insertAssembly
+csgFromList op =
+    \assembly ->
+        case assembly of
+            [] ->
+                A.Empty
+
+            x :: xs ->
+                List.foldl (A.CSG G.aabbAll [] op) x xs
